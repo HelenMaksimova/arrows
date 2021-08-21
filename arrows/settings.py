118d6f361d2b4b279cd939ca316708f83dca8f4b
helen <alnorda@gmail.com>
2021-08-21 22:28:51 +0300

    lesson_01 renewed settings and requirements (drf installed), added users app

--- arrows/settings.py
+++ arrows/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3-d party applications
+    'rest_framework',
+    # project applications
+    'users',
 ]
 
 MIDDLEWARE = [
