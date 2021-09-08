11e256f0c63357105eb48d139a2c2f96faba6882
helen <alnorda@gmail.com>
2021-09-08 19:55:21 +0300

    lesson_06 added authentication and permissions

--- arrows/settings.py
+++ arrows/settings.py
@@ -155,6 +155,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # оставлено для проверки прав
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ]
